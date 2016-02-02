7a40a7b452bd51c52ded7224cef905147b2217f5
borislin <boruilin224@gmail.com>
2016-02-02 11:23:19 -0800

    Update README

--- notes.js
+++ notes.js
@@ -10,3 +10,23 @@ console.log("hello")
 === "equal to"
 !== "not equal to"
 
+
+
+// Chapter 1: The ABC of Programming
+
+// Page 39
+document.write()
+document.getElementById()
+
+// Page 51
+put JavaScript at the end of the body section of an HTML page can significantly reduce the loading time
+
+// Chapter 2: Basic JavaScript Instructions
+
+// Page 65
+document.getElementById().innerHTML
+In certain cases, this property can pose a security risk
+
+// Page 66
+document.getElementById().className
+
